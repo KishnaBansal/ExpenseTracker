52d8aa9a0f1acd8dbb84a0021f90f946461c5adc
Kishna Bansal <kb.20051981@gmail.com>
2024-01-20 22:58:44 +0530

    Learned about DateTime datstype,enum,uuid package and made a new dummy list of expenses

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -8,6 +9,20 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      date: DateTime.now(),
+      title: "Flutter Course",
+      amount: 439,
+      category: Category.work,
+    ),
+    Expense(
+      date: DateTime.now(),
+      title: "Cinema",
+      amount: 200,
+      category: Category.leisure,
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
