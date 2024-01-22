7d2174867525c9b8c35560f19f80f0fb4dc1a271
Kishna Bansal <kb.20051981@gmail.com>
2024-01-23 00:05:35 +0530

    moved some files to make the project managble. Used Card widget and made a basic layout of the home page.

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,36 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.access_time_filled_sharp),
+                    const SizedBox(width: 8),
+                    Text(expense.date.toString()),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
