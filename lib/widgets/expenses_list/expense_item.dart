3d476603dc00ae50763573883758795a39867f8c
Kishna Bansal <kb.20051981@gmail.com>
2024-01-23 00:52:36 +0530

    made different icons and learned about getters and used intl package for formatted date and then made some changes in calling these on screen.

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -21,9 +21,9 @@ class ExpenseItem extends StatelessWidget {
                 const Spacer(),
                 Row(
                   children: [
-                    const Icon(Icons.access_time_filled_sharp),
+                    Icon(categoryIcons[expense.category]),
                     const SizedBox(width: 8),
-                    Text(expense.date.toString()),
+                    Text(expense.formattedDate),
                   ],
                 ),
               ],
