e9090bc1307ee3a3e569da88f92acafa7d03a7b4
Kishna Bansal <kb.20051981@gmail.com>
2024-01-23 22:43:19 +0530

    made appBar and made the add icon to work

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -24,9 +24,28 @@ class _ExpensesState extends State<Expenses> {
       category: Category.leisure,
     )
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const Text("Modal overlayed sheet"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter ExpenseTracker"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _openAddExpenseOverlay();
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text("Chart"),
