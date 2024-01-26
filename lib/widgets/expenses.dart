85e115913cfd1750ba4eaaaf4c302b56f625c90b
Kishna Bansal <kb.20051981@gmail.com>
2024-01-26 16:01:30 +0530

    made the overlay to cover full screen and enabled the swipe feature of the app by managing the internal data along with it to avoid any errors

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -28,6 +28,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
@@ -36,6 +37,12 @@ class _ExpensesState extends State<Expenses> {
   void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
     });
   }
 
@@ -57,7 +64,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text("Chart"),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(expenses: _registeredExpenses,onRemoveExpense: _removeExpense),
           ),
         ],
       ),
